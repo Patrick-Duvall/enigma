9473e83393412c23be7af6f471f25daa29f20eb6
Patrick Duvall <duvallp208@gmail.com>
2019-04-11 15:23:44 -0600

    Fix bug in keygenerator make master method looping on numbers less that 10000

--- spec/offset_generator_spec.rb
+++ spec/offset_generator_spec.rb
@@ -1,7 +1,7 @@
 require "./spec/spec_helper"
 
   describe OffsetGenerator do
-
+    
     it 'generates 4 offsets using the date' do
       expect([5,5,6,1]).to eq(OffsetGenerator.generate('110419'))
     end
