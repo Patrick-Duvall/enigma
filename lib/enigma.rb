4fe7639567744133771aeaa62be1d25989531ee9
Patrick Duvall <duvallp208@gmail.com>
2019-04-11 14:31:46 -0600

    Start Enigma method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,6 +3,9 @@ require "./spec/spec_helper"
 
 class Enigma
 
-  
+  def encrypt(string, masterkey, ddmmyy)
+    keys = KeyGenerator.generate(masterkey)
+    offsets = OffsetGenerator.generate(ddmmyy)
+  end
 
 end
