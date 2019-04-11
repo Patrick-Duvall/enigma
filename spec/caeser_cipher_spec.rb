06e8d0a3f1470ea08797c0de90dde5b3add8e261
Patrick Duvall <duvallp208@gmail.com>
2019-04-11 13:44:30 -0600

    Add enigma encrypt test

--- spec/caeser_cipher_spec.rb
+++ spec/caeser_cipher_spec.rb
@@ -7,9 +7,7 @@ require "./spec/spec_helper"
     end
 
     it 'ciphers a single letter' do
-
       expect('c').to eq(@cipher.rotate('a', 2))
-
     end
 
     it 'will include spaces' do
@@ -17,13 +15,9 @@ require "./spec/spec_helper"
       expect(' ').to eq(cipher.rotate('w', 0))
     end
 
-
     it 'rotates back around to a' do
       cipher = CaeserCipher.new(4)
       expect('a').to eq(cipher.rotate('w', 1))
-
-  end
-
-
+    end
 
   end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -3,10 +3,20 @@ require "./spec/spec_helper"
 
 describe Enigma do
 
+  it 'exists' do
+    enigma = Enigma.new
+    expect(enigma).to be_an(Enigma)
+  end
 
-it 'exists' do
-  enigma = Enigma.new
-  expect(enigma).to be_an(Enigma)
-end
+  it 'encrypts a message with key and date' do
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    enigma = Enigma.new
+    expect(expected).to eq(enigma.encrypt("hello world", "02715", "040895"))
+
+  end
 
 end
