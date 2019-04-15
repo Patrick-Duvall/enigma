81733dc8129c75902c5d98dcfeb2768074cbe422
Patrick Duvall <duvallp208@gmail.com>
2019-04-15 09:33:35 -0600

    Refactor key generator

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,13 +1,11 @@
 class KeyGenerator
 
   def self.generate(master = self.make_master)
-    keys = [master[0..1], master[1..2],master[2..3],master[3..4]]
-    keys.map(&:to_i)
+    keys = [master[0..1], master[1..2],master[2..3],master[3..4]].map(&:to_i)
   end
 
   def self.make_master
-    master = rand(1..99999)
-    master = master.to_s
+    master = rand(1..99999).to_s
     master = ('0' + master) until master.length == 5
     master
   end
