46b44ad7c84b686fedc260000eba1f101a24d902
Patrick Duvall <duvallp208@gmail.com>
2019-04-15 13:46:45 -0600

    Add minor comment

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -5,6 +5,7 @@ class KeyGenerator
     # keys = [master[0..1], master[1..2],master[2..3],master[3..4]]
     # keys.map(&:to_i)
     master.to_s.chars.each_cons(2).to_a.map{|el|el[0] + el[1]}.map(&:to_i)
+          #^^^^
   end
 
   def self.make_master
