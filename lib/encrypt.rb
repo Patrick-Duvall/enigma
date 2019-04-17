f920999e21b922e4fd83c5fe8b8e139b20f6009d
Patrick Duvall <duvallp208@gmail.com>
2019-04-17 08:51:27 -0600

    Make encrypt decrypt actually work

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -10,10 +10,12 @@ output = ARGV[1]
 
 to_encrypt = File.read(ARGV[0])
 
+
 enigma = Enigma.new
 master_key = KeyGenerator.make_master
 date =  OffsetGenerator.convert_date
 encrypted = enigma.encrypt(to_encrypt, master_key, date)[:encryption]
+
 
 File.write(output, encrypted)
 puts "Created '#{output}' with the key #{master_key} and date #{date}"
